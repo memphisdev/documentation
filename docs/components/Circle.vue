312834665b5929714445b6670a0c1774caebeed4
FriendlyBandit <john@memphis.dev>
2023-08-14 14:12:05 -0500

    "Changed light theme for the Circle vue component"

--- docs/components/Circle.vue
+++ docs/components/Circle.vue
@@ -9,10 +9,14 @@
 
 <style scoped>  
     span{
-        background-color: #313136;
+        background-color: var(--m-light-highlight);
         width:  50px;
         height:  25px;
         padding: 1px 10px 1px 10px;
         border-radius: 32px;
+    }
+
+    :root.dark span{
+        background-color: #313136;
     }
 </style>
